c1e8b583f6b43073757af69feab4d19c18f89fe2
sinius15 <sinius15@gmail.com>
2014-08-01 15:10:36 +0200

    automatic git pusher test #2 	modified:   src/com/sinius15/cardsim/CardSimulator.java

--- src/com/sinius15/cardsim/CardSimulator.java
+++ src/com/sinius15/cardsim/CardSimulator.java
@@ -3,7 +3,7 @@ package com.sinius15.cardsim;
 public class CardSimulator {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello Github!");
+		System.out.println("Hello Github! #nr2");
 	}
 	
 }
